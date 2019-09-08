a436ca7030d0b1ffb372797f3b7c1963d252e426
Kirill Bogatikov <kivi15penza@gmail.com>
2019-09-08 16:38:46 +0300

    Added isDefault method to NeoPlatform

--- neofit/src/main/java/org/cuba/neofit/NeoPlatform.java
+++ neofit/src/main/java/org/cuba/neofit/NeoPlatform.java
@@ -1,8 +1,10 @@
 package org.cuba.neofit;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 
 public abstract class NeoPlatform {
     public abstract Executor requestExecutor();
     public abstract Executor callbackExecutor();
+    public abstract boolean isDefault(Method method);
 }
